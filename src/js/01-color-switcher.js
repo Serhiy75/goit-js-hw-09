b0b9f2746f38a04b7890ea41e35c56ca20206644
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-12 00:40:18 +0300

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let intervalId = null;
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function changeColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+};
+
+btnStart.addEventListener('click', () => {
+    if (!intervalId) {
+        btnStart.disabled = true;
+        intervalId = setInterval(changeColor, 1000);
+    }
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(intervalId);
+    btnStart.disabled = false;
+    intervalId = null;
+    btnStop.disabled = true;
+});
